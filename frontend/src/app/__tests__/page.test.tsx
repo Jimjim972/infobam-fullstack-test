5db84a56c18f8d7ccf900d0a4d6c4981a76cc9fe
Jimjim972 <pavade.jimmy@gmail.com>
2025-05-17 14:15:33 +0200

    page test passe just for init

--- frontend/src/app/__tests__/page.test.tsx
+++ frontend/src/app/__tests__/page.test.tsx
@@ -9,7 +9,7 @@ describe("Home", () => {
     render(<Home />);
 
     const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
+      name: /Go to nextjs\.org!/i,
     });
 
     expect(heading).toBeDefined();
